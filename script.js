807a10b5917b939c943c1024a6665d27e7adfc88
featheronlya <featheronlya@gmail.com>
2026-04-06 16:42:40 +0800

    Initial upload of sound map files

---
+++ script.js
@@ -0,0 +1,45 @@
+// 1. 初始化地圖，設為深色風格 (使用 CartoDB 提供的免費深色地圖)
+const map = L.map('map').setView([35.0, 135.7], 7); 
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    attribution: '© OpenStreetMapers'
+}).addTo(map);
+
+// 2. 定義你的聲音數據庫
+const locations = [
+    {
+        name: "伏見稻荷・流水",
+        coords: [34.9671, 135.7727],
+        audio: "audio/fushimi.mp3",
+        image: "images/fushimi.jpg",
+        desc: "清晨泉水冒出的咕嘟聲，帶著苔蘚的濕氣。"
+    },
+    {
+        name: "高松・海風",
+        coords: [34.3503, 134.0465],
+        audio: "audio/takamatsu.mp3",
+        image: "images/takamatsu.jpg",
+        desc: "瀨戶內海的平靜海風，夾雜著遠處的船笛。"
+    },
+    // ... 依此類推加入金澤、奈良、宇治
+];
+
+// 3. 將點位加入地圖
+locations.forEach(loc => {
+    const marker = L.marker(loc.coords).addTo(map);
+    
+    marker.on('click', () => {
+        // 更新郵票卡片內容
+        document.getElementById('pc-title').innerText = loc.name;
+        document.getElementById('pc-desc').innerText = loc.desc;
+        document.getElementById('pc-image').src = loc.image;
+        document.getElementById('pc-coord').innerText = `LOC: ${loc.coords[0]}, ${loc.coords[1]}`;
+        
+        const player = document.getElementById('audio-player');
+        player.src = loc.audio;
+        player.play();
+        
+        document.getElementById('postcard').style.display = 'block';
+        map.flyTo(loc.coords, 13); // 點擊後地圖平滑縮放至該處
+    });
+});
